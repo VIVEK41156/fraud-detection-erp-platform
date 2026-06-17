2b44ef27fbb4cd5a06896ac4dbae1699c5a26b8e
VIVEK41156 <vivekparri41156@gmail.com>
2026-06-17 13:50:07 +0530

    Update frontend API URL

--- fraud-detection-frontend/src/services/api.js
+++ fraud-detection-frontend/src/services/api.js
@@ -4,7 +4,6 @@ const API = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 });
 
-// Auto attach token
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
