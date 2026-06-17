814d902f8dc01ac5be9ad0be1657604086059891
VIVEK41156 <vivekparri41156@gmail.com>
2026-06-17 12:34:46 +0530

    Configure frontend for Render deployment

--- fraud-detection-frontend/src/services/api.js
+++ fraud-detection-frontend/src/services/api.js
@@ -1,32 +1,21 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Auto attach token
 API.interceptors.request.use(
   (config) => {
-    const token =
-      localStorage.getItem(
-        "token"
-      );
-
-    console.log(
-      "TOKEN:",
-      token
-    );
+    const token = localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   },
-  (error) =>
-    Promise.reject(error)
+  (error) => Promise.reject(error)
 );
 
 export default API;
